e968eb4ca7b2d86d637272aef7717ec24e73eae1
hwelch-fle <hwelch@finelines-engineering.com>
2026-02-17 19:42:19 -0500

    more simple layout of VariableStore

--- src/signified/lazy.py
+++ src/signified/lazy.py
@@ -1,15 +1,15 @@
 from collections import defaultdict, deque
-from collections.abc import Generator, Callable
+from collections.abc import Generator, Callable, Iterator
 from typing import Any
 
 from .core import Observer, Variable
 from .types import _OrderedWeakrefSet as OWRS, HasValue
 
 
-type DepMap = defaultdict[int, OWRS[Observer]]
+type DepMap = defaultdict[int, OWRS[Variable[Any]]]
+
 
 def changed(was: Any, now: Any) -> bool:
-    # Same object
     if was is now:
         return False
     
@@ -24,27 +24,69 @@ def changed(was: Any, now: Any) -> bool:
     try:
         return bool(not_equal)
     except ValueError:
+        # numpy array.any()
         return bool(getattr(not_equal, 'any', lambda: False)())
     return False
 
-class ComputeContext:
-    def __init__(self, *, max_stack: int| None = None) -> None:
-        self.max_stack = max_stack
+class VariableStore:
+    """Container for tracking state of the current namespace
+    
+    Usage:
+        ```py
+        >>> from sigified import _STORE, Signal
+        >>> 
+        >>> s = Signal([1, 2, 3])
+        >>> y = s.rx.map(lambda i: i**2)
+        >>> print(_STORE)
+        Tracking: 2 Variables
+        ```
+    """
+    def __init__(self, *, max_deps: int| None = None) -> None:
+        self.max_stack = max_deps
         self.tracked = OWRS[Variable[Any]]()
-        self.dep_map: DepMap = defaultdict(OWRS[Observer])
+        self.dep_map: DepMap = defaultdict(OWRS[Variable[Any]])
+        self.stack = deque[Variable[Any]](maxlen=max_deps)
+
+    def __repr__(self) -> str:
+        return f'Tracking: {len(self.tracked)} Variables'
 
     def track(self, variable: Variable[Any]) -> None:
         self.tracked.add(variable)
-        self.dep_map[id(variable)] = variable._observers
+        # NOTE: variable._observers is only referenced
+        # If the variable mutates this set, changes will be reflected here.
+        # This is likely needs to be managed with a special threadsafe implementation
+        # if at any point we want to support async/paralell updates 
+        self.dep_map[id(variable)] = variable._observers # type: ignore (observers should all support Variable interface)
 
-    def sync_context(self, variable: Variable[Any]) -> None:
+    def _get_deps(self, variable: Variable[Any]) -> Iterator[Variable[Any]]:
+        self._prevent_circular(variable)
         for dep in self.dep_map.get(id(variable), []):
-            dep.update()
+            yield dep
 
+    def _prevent_circular(self, variable: Variable[Any]) -> None:
+        # prevent infinite looping
+        # e.g.
+        # >>> x = Signal(1)
+        # >>> y = Signal(x)
+        # >>> x.value = y
+        # RecursionError ...
+        for dep in self.dep_map.get(id(variable), []):
+            if id(dep) == id(variable):
+                raise RecursionError(f'{dep}@{id(dep)} is dependant on self!')
+    
     def refresh(self, variable: Variable[Any]) -> None:
-        _stack = deque[Variable[Any]](maxlen=self.max_stack)
-        _stack.append(variable)
-        _deps = self.dep_map.get(id(variable), [])
-        if not _deps:
-            return
-        
+        """Refresh the dependency tree for the provided Variable"""
+        try:
+            # get deps
+            self.stack.extend(self._get_deps(variable))
+            # recursively refresh deps
+            while self.stack:
+                self.refresh(self.stack.pop())
+            # detect change
+            if changed(variable._value, variable.value):
+                # notify subscribers
+                variable.notify()
+        finally:
+            # Ensure that the stack is cleared if the 
+            # refresh fails
+            self.stack.clear()
